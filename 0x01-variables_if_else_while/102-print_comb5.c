d6eaa6e5fe3b1fae6f6a12efdbd4fd21a792d9be
Augustine Omenuwa <elitetech7827@gmail.com>
2023-02-21 08:19:45 -0800

    add

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -24,10 +24,10 @@ int main(void)
 				putchar(' ');
 				putchar((b / 10) + 48);
 				putchar((b % 10) + 48);
-				putchar(',');
-				putchar(' ');
 				if (a == 98 && b == 99)
 					break;
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
